d43da781e2b6208cbd7d6d94d7041036451498d3
ToroDanger <bryanppmg90@gmail.com>
2024-09-27 19:46:17 -0300

    Avances

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { UserService } from './services/user.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
 })
+
 export class AppComponent {
-  constructor() {}
+  name: string ='';
+  age: number | null = null;
+  users$: Observable<any[]>;
+
+  constructor(private userService: UserService) {
+    this.users$ = this.userService.getUsers();
+  }
+  agregar(){
+    if (this.name && this.age !== null){
+      this.userService.addUser(this.name, this.age).then(() => {
+        console.log('User added successfully!');
+        this.name='';
+        this.age= null;
+      })
+    }
+  }
 }
